6df8237358f0fb2f1baf203e6c509b0cb8288cc0
rajaifi91 <rajaifi91@yahoo.com>
2025-03-09 17:58:44 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import '@heartlandone/vega/style/vega.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
-//import router from 'vue-router'
+//import router from 'vue-router';
 
 
 import { VegaComponentLibrary } from '@heartlandone/vega-vue';
 const app = createApp(App).use(VegaComponentLibrary);
 VegaComponentLibrary.isReady().then( ()=>
-{   
+{  
  //app.use(router);
  app.mount("#app"); 
 });
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import About from '../views/About.vue';
+import About from '. ./views/About.vue';
 import Contact from '../views/Contact.vue';
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 
